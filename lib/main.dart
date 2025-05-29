abc0da41afd0bab80883d754f960c8a2893714b2
mocu. <pranav.22ai033@sode-edu.in>
2025-05-29 19:02:59 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MaterialApp(home: SplashScreen(),));
+}
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.sizeOf(context).height,
+      width: MediaQuery.sizeOf(context).width,
+      child: ,
+    );
+  }
+}
